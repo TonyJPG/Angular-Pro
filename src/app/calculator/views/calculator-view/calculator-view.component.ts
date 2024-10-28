09a7e9f80453671ac326dfa7b95fe93adf437b10
Antonio Pérez <jpgtony@gmail.com>
2024-10-28 13:12:02 +0100

    added primeng

--- src/app/calculator/views/calculator-view/calculator-view.component.ts
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -1,11 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { CalculatorComponent } from '../../components/calculator/calculator.component';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'calculator-view',
   standalone: true,
-  imports: [CalculatorComponent],
+  imports: [CalculatorComponent, ButtonModule],
   templateUrl: './calculator-view.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class CalculatorViewComponent {}
+export default class CalculatorViewComponent implements OnInit {
+  private primengConfig = inject(PrimeNGConfig);
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+}
